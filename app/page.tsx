4b7f6667b2da7afb86daf88516434f9c15781c68
Taraka2027 <mhtaraka2027@gmail.com>
2025-03-07 14:54:03 +0700

    Penyesuaian Hero

--- app/page.tsx
+++ app/page.tsx
@@ -10,28 +10,26 @@ export default function Home() {
       <Navbar />
       <div className="hero-container w-screen text-black h-screen pt-5">
         <div className="hero-box w-screen bg-[#F2F9FF] h-full flex justify-center items-center">
-          <div className="w-[89%] h-[70%] flex flex-col md:flex-row items-center justify-between">
-            <section className="h-[68%] flex flex-col justify-between">
-              <h1 className="text-[60px] font-semibold leading-24">
+          <div className="w-[86%] h-[70%] flex flex-col md:flex-row items-center justify-between">
+            <section className="h-[62%] flex flex-col justify-between mb-12">
+              <h1 className="text-[68px] font-semibold leading-20">
                 Teman Setia Pengelola <br></br>
                 <span className="text-[#089BFF] font-bold">Keuangan</span> Anda
               </h1>
-              <h2 className="text-3xl font-normal leading-12 py-10">
+              <h2 className="text-[26px] font-normal leading-10 py-10">
                 Website yang siap jadi teman dalam mengelola <br></br>
                 keuangan pribadi.
               </h2>
               <a
-                className="bg-[#089BFF] text-white text-3xl font-medium py-4 px-10 rounded-2xl w-fit"
+                className="bg-[#089BFF] text-white text-[28px] font-medium py-3 px-7 rounded-2xl w-fit"
                 href="#"
               >
                 Get Started
               </a>
             </section>
-            <Image
+            <img
               src="/hero.png"
               alt="Hero Image"
-              width={500}
-              height={500}
               className="w-[35%] mt-10 md:mt-0"
             />
           </div>
